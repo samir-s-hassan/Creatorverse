d3c67b1c9050d1f56c5f67ac9dae63c4f68ec3ba
samir-s-hassan <hsamir347@gmail.com>
2024-08-03 13:32:35 -0400

    created structure of app and initialized connection to supabase database

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useRoutes } from 'react-router-dom'
 import './App.css'
 
 function App() {
---
+++ src/client.jsx
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+const URL = 'https://fmdmbzjndaoowdujfkwr.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZG1iempuZGFvb3dkdWpma3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5Mjc3MTQsImV4cCI6MjAzNzUwMzcxNH0.KOkOO_pms7DaMCjerMH-T6NlKsMXysm7AI0W7pMnWZo';
+
+export const supabase = createClient(URL, API_KEY);
