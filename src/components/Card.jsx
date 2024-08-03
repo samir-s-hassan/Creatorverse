5824cb88cf05177e256d96d8922a08e0c71f4765
samir-s-hassan <hsamir347@gmail.com>
2024-08-03 14:41:33 -0400

    added an edit and view button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css'; // Import the CSS file
 
-const Card = ({ name, url, description, imageURL, }) => {
+const Card = ({ id, name, url, description, imageURL }) => {
   return (
     <div className="card">
-      <img src={imageURL} alt={`${name}'s image`} className="card-image" />
+      <div className="card-image-container">
+        {imageURL ? (
+          <img src={imageURL} alt={`${name}'s image`} className="card-image" />
+        ) : (
+          <p className="no-image-text">No Image Available</p>
+        )}
+      </div>
       <div className="card-content">
         <h2 className="card-title">{name}</h2>
         <p className="card-description">{description}</p>
         <a href={url} className="card-url">Visit Profile</a>
+        <div className="card-buttons">
+          <Link to={`/edit/${id}`} className="card-button card-edit-button">Edit</Link>
+          <Link to={`/view/${id}`} className="card-button card-view-button">View</Link>
+        </div>
       </div>
     </div>
   );
